d55a7f83f9c06f04b5c54155128458718f58ebd9
Satyabrata sahoo <satyabratasahoo02001@gmail.com>
2026-09-18 21:15:46 +0530

    Add T-Cart online shopping project

---
+++ script.js
@@ -0,0 +1,19 @@
+function buyNow() {
+    alert("Welcome to TechKart! Start Shopping.");
+}
+
+function login() {
+    alert("Login Successful");
+}
+
+function addToCart() {
+    alert("Product Added to Cart Successfully!");
+}
+
+function checkout(){
+    alert("Proceeding to Checkout");
+}
+
+function goHome(){
+    window.location.href="index.html";
+}
